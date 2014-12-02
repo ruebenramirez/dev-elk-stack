675db26840c84853a0bf3c21f4931c914148d55c
Rueben Ramirez <ruebenramirez@gmail.com>
2014-12-02 20:35:53 +0000

    elasticsearch + kibana (w/ link to es)

--- init-elk-docker.sh
+++ init-elk-docker.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
-# https://registry.hub.docker.com/u/qnib/elk/
+docker pull dockerfile/elasticsearch
+docker pull balsamiq/docker-kibana
 
-docker pull qnib/elk
-
-docker rm -f elk
-
-# To get all the /dev/* devices needed for sshd and alike:
-export DEV_MOUNTS="-v /dev/null:/dev/null -v /dev/urandom:/dev/urandom -v /dev/random:/dev/random"
-export DEV_MOUNTS="${DEV_MOUNTS} -v /dev/full:/dev/full -v /dev/zero:/dev/zero"
-### OPTIONAL -> if you want to store Elasticsearchs data outside 
-mkdir ${HOME}/elasticsearch
-export ES_PERSIST="-v ${HOME}/elasticsearch:/var/lib/elasticsearch"
-### OPTIONAL -> set the external port to something else then 80
-export HTTP_PORT="-e HTTPPORT=8080 -p 8080:80"
-### OPTIONAL -> To secure kibana and elasticsearch user/passwd could be set
-# if a user is set and no passwd, the user will be set as password
-export HTUSER=kibana
-export HTPASSWD=secretpw
+docker rm -f elasticsearch
+docker rm -f kibana
 
 docker run -d \
-    -h elk \
-    --name elk \
-    --privileged \
-    ${DEV_MOUNTS} \
-    ${HTTP_PORT} \
-    -e HTUSER=${HTUSER} \
-    -e HTPASSWD=${HTPASSWD} \
-    ${ES_PERSIST} \
-    qnib/elk:latest
+    -p 9200:9200 \
+    -p 9300:9300 \
+    --name elasticsearch \
+    dockerfile/elasticsearch
 
-echo 'message2' | nc -w 1  192.168.59.103 5514
+docker run -d \
+    -p 9888:80 \
+    -e KIBANA_SECURE=false \
+    --link elasticsearch:es \
+    --name kibana \
+    balsamiq/docker-kibana
 
-docker logs -f elk
